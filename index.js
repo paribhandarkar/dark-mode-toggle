de005331fc8f403efe252343e2566a59374cff16
paribhandarkar <76446574+paribhandarkar@users.noreply.github.com>
2023-02-27 22:14:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const inputEl = document.querySelector(".input");
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+updateBody();
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.backgroundColor = "plum";
+  } else {
+    bodyEl.style.backgroundColor = "pink";
+  }
+}
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
